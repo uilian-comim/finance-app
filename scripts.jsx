60e8c2b6e3682b6f5724036314a88f0b8dc9cd1d
Eorihs <pikapau.comim@gmail.com>
2021-08-11 08:13:32 -0300

    Fourth commit Added transition edit function, added theme change function, and updated the styling of some components.

--- scripts.jsx
+++ scripts.jsx
@@ -1,4 +1,29 @@
+function loadTheme() {
+   if (JSON.parse(localStorage.getItem("dark-theme")) == "true") {
+      Modal.DarkTheme.activeDarkTheme();
+   }
+   else {
+
+   }
+}
+
 let Modal = {
+   DarkTheme: {
+      activeDarkTheme() {
+         const body = document.querySelector('body');
+         body.classList.add("dark-theme");
+      }
+   },
+   changeTheme() {
+      const body = document.querySelector('body')
+      if (body.classList.contains("dark-theme")) {
+         body.classList.remove("dark-theme");
+         localStorage.setItem("dark-theme", JSON.stringify("false"));
+      }else {
+         body.classList.add("dark-theme");
+         localStorage.setItem("dark-theme", JSON.stringify("true"));
+      }
+   },
    Toogle() {
       const modalOverlay = document.querySelector(".modal-overlay");
 
@@ -8,15 +33,51 @@ let Modal = {
       else {
          modalOverlay.classList.add("active");
       }
+   },
+   ToogleEdit() {
+      const modalOverlay = document.querySelector(".modal-overlay-edit");
+
+      if (modalOverlay.classList.contains("active")) {
+         modalOverlay.classList.remove("active");
+      }
+      else {
+         modalOverlay.classList.add("active");
+      }
+   },
+   EditTransaction(index) {
+      Modal.ToogleEdit();
+      const transactionContainer = {
+         description: Transaction.all[index].description,
+         amount: Transaction.all[index].amount,
+         date: Transaction.all[index].date,
+         typeTransaction: Transaction.all[index].typeTransaction,
+      }
+
+      const descriptionInput = document.querySelector("#description-edit");
+      const pay = document.querySelector("#pay-edit");
+      const receive = document.querySelector("#receive-edit");
+      const amount = document.querySelector("#amount-edit");
+      const date = document.querySelector("#date-edit");
+
+      if (transactionContainer.typeTransaction == "pay") {
+         pay.checked = true;
+      }
+      else if (transactionContainer.typeTransaction == "receive") {
+         receive.checked = true;
+      }
+
+      date.value = Utils.defaultDate(transactionContainer.date);
+      descriptionInput.value = transactionContainer.description;
+      amount.value = transactionContainer.amount / 100;
    }
 }
 
 const Storage = {
    get() {
-      return JSON.parse(localStorage.getItem("dev.finances:transactions")) || []
+      return JSON.parse(localStorage.getItem("dev_finances_transactions")) || []
    },
    set(transactions) {
-      localStorage.setItem("dev.finances:transactions", JSON.stringify(transactions))
+      localStorage.setItem("dev_finances_transactions", JSON.stringify(transactions))
    }
 }
 
@@ -28,10 +89,19 @@ const Transaction = {
       App.reload();
    },
 
+   addEdit(index, transaction) {
+      Transaction.all[index] = transaction;
+      App.reload();
+   },
+
    remove(index) {
       Transaction.all.splice(index, 1);
 
       App.reload();
+   },
+   edit(index) {
+      Utils.index = index;
+      Modal.EditTransaction(index);
    },
    incomes() {
       let income = 0;
@@ -56,6 +126,13 @@ const Transaction = {
       return expense
    },
    total() {
+      if (Transaction.expenses() > Transaction.incomes()) {
+         document.querySelector(".card.total").classList.add("red");
+      } else {
+         if (document.querySelector(".card.total").classList.contains("red")) {
+            document.querySelector(".card.total").classList.remove("red");
+         }
+      }
       return Transaction.incomes() - Transaction.expenses();
    }
 }
@@ -80,8 +157,11 @@ const DOM = {
          <td class="${CSSclass}">${amount}</td>
          <td class="date">${transaction.date}</td>
          <td>
-            <img onclick="Transaction.remove(${index})" src="./assets/minus.svg" alt="Remover transação">
-         </td>  
+            <img onclick="Transaction.edit(${index})" title="Editar transação" src="./assets/edit.svg" alt="Editar transação">
+         </td>
+         <td>
+            <img onclick="Transaction.remove(${index})" title="Remover transação" src="./assets/minus.svg" alt="Remover transação">
+         </td>
       `
 
       return html
@@ -97,7 +177,7 @@ const DOM = {
       messageError.classList.add(`${type}`);
       progressBar.classList.add(`${type}`);
       messageH3.innerHTML = `${message}`;
-      setTimeout(() => { messageError.classList.remove(`${type}`); imgMessage.setAttribute("src", ""); progressBar.classList.remove(`${type}`); messageH3.innerHTML = ""}, 10000)
+      setTimeout(() => { messageError.classList.remove(`${type}`); imgMessage.setAttribute("src", ""); progressBar.classList.remove(`${type}`); messageH3.innerHTML = "" }, 10000)
    },
 
    updateBalance() {
@@ -111,9 +191,14 @@ const DOM = {
 }
 
 const Utils = {
+   index: "",
    formatDate(date) {
       const splittedDate = date.split("-")
       return `${splittedDate[2]}/${splittedDate[1]}/${splittedDate[0]}`
+   },
+   defaultDate(date) {
+      const defaultDate = date.split("/");
+      return defaultDate[2] + "-" + defaultDate[1] + "-" + defaultDate[0];
    },
    formatAmount(value) {
       value = Number(value) * 100
@@ -159,13 +244,13 @@ const Form = {
       if (description.trim() === "") {
          DOM.messageBox("error", "Defina uma descrição ao item para continuar.");
          throw new Error("Preencha todos os campos.");
-      }else if (amount.trim() === "") {
+      } else if (amount.trim() === "") {
          DOM.messageBox("error", "Defina uma quantidade de dinheiro para continuar.");
          throw new Error("Preencha todos os campos.");
-      }else if (date === "") {
+      } else if (date === "") {
          DOM.messageBox("error", "Selecione a data do pagamento/recebimento para continuar.");
          throw new Error("Preencha todos os campos.");
-      }else if (typeTransaction == "") {
+      } else if (typeTransaction == "") {
          DOM.messageBox("error", "Selecione se você está pagando algo/alguém ou recebendo o dinheiro para continuar.");
          throw new Error("Preencha todos os campos.");
       }
@@ -190,11 +275,78 @@ const Form = {
          Form.validateFields();
          const transaction = Form.formatValues();
          Transaction.add(transaction);
-         DOM.messageBox("success",  `Item ${transaction.description} adicionado com sucesso.`)
+         DOM.messageBox("success", `Item ${transaction.description} adicionado com sucesso.`)
          Form.clearFields();
          Modal.Toogle();
       } catch (error) {
 
+      }
+   }
+}
+
+const FormEdit = {
+   description: document.querySelector("input#description-edit"),
+   amount: document.querySelector("input#amount-edit"),
+   date: document.querySelector("input#date-edit"),
+   getTypeTransaction() {
+      const typeTransaction = document.querySelector("input#pay-edit");
+      if (typeTransaction.checked) {
+         return typeTransaction;
+      }
+      else {
+         return document.querySelector("#receive-edit");
+      }
+   },
+   formatValues() {
+      let { description, amount, date, typeTransaction } = FormEdit.getValues()
+
+      amount = Utils.formatAmount(amount);
+
+      date = Utils.formatDate(date);
+
+      return { description, amount, date, typeTransaction }
+   },
+   getValues() {
+      return {
+         description: FormEdit.description.value,
+         amount: FormEdit.amount.value,
+         date: FormEdit.date.value,
+         typeTransaction: FormEdit.getTypeTransaction().value,
+      }
+   },
+   validateFields() {
+      const { description, amount, date, typeTransaction } = FormEdit.getValues();
+      if (description.trim() === "") {
+         DOM.messageBox("error", "Defina uma descrição ao item para continuar.");
+         throw new Error("Preencha todos os campos.");
+      } else if (amount.trim() === "") {
+         DOM.messageBox("error", "Defina uma quantidade de dinheiro para continuar.");
+         throw new Error("Preencha todos os campos.");
+      } else if (date === "") {
+         DOM.messageBox("error", "Selecione a data do pagamento/recebimento para continuar.");
+         throw new Error("Preencha todos os campos.");
+      } else if (typeTransaction == "") {
+         DOM.messageBox("error", "Selecione se você está pagando algo/alguém ou recebendo o dinheiro para continuar.");
+         throw new Error("Preencha todos os campos.");
+      }
+   },
+   clearFields() {
+      FormEdit.description.value = ""
+      FormEdit.amount.valque = ""
+      FormEdit.date.value = ""
+   },
+   submit(event) {
+      event.preventDefault();
+      try {
+         FormEdit.validateFields();
+         const transaction = FormEdit.formatValues();
+         Transaction.addEdit(Utils.index, transaction);
+         DOM.messageBox("success", `Item ${transaction.description} editado com sucesso.`);
+         FormEdit.clearFields();
+         Modal.ToogleEdit();
+      }
+      catch (error) {
+         console.log(error);
       }
    }
 }
@@ -204,7 +356,6 @@ const App = {
       Transaction.all.forEach(DOM.addTransaction);
 
       DOM.updateBalance();
-
       Storage.set(Transaction.all);
    },
    reload() {
